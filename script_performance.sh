cbdb6f2faf6c423e8e6a5120835fa37d3aab36d1
sriram <sriram.katta@fau.de>
2024-08-04 14:13:39 +0200

    chore: Disable likwid performance measurements in script_performance.sh

--- script_performance.sh
+++ script_performance.sh
@@ -4,7 +4,7 @@ module load intel likwid
 
 make clean
 
-likwid=on CXX=icpx make > /dev/null
+likwid=off CXX=icpx make > /dev/null
 
 srun --cpu-freq=2000000-2000000 likwid-pin -q -c M0:0-17 ./perf 5000 5000
 
