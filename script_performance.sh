d5012974e15fa84f2025a7905f4184f937248810
sriram <sriram.katta@fau.de>
2024-08-18 19:20:20 +0200

    updated performance script

--- script_performance.sh
+++ script_performance.sh
@@ -6,6 +6,6 @@ make clean
 
 likwid=off CXX=icpx make > /dev/null
 
-srun --cpu-freq=2000000-2000000 likwid-pin -q -c M0:0-17 ./perf 5000 5000
+srun --cpu-freq=2000000-2000000:performace likwid-pin -q -c M0:0-17 ./perf 5000 5000
 
 make clean
