6597e421b12b07b8ede461706d8a6a383d67c05e
sriram <sriram.katta@fau.de>
2024-09-04 18:25:06 +0200

    update

--- script_codebalance_1ccnuma.sh
+++ script_codebalance_1ccnuma.sh
@@ -17,18 +17,26 @@ module load likwid
 
 make clean
 
-#LIKWID=on CXX=icpx make -j > /dev/null
+DATE=$(date +'%d-%m-%y_%H@%M@%S')
+perffile=perf_$DATE
+
+LIKWID=on CXX=icpx make -j > /dev/null
+
+mv perf $perffile
 
 region=""
 intensity=""
 
+finalres=./codebalance_data/resfile
+> $finalres
+
 for simrange in "2000 20000" "20000 2000" "1000 400000"
 do
     outfilename=./codebalance_data/file$(echo $simrange | awk '{print $(NF)}')
-    srun --cpu-freq=2000000-2000000:performance likwid-perfctr -m -C M0:0-17 -g MEM_DP ./perf $simrange > ./ codebalancedata/$outfilename
-echo "---------------------------------------------------------------------------------------"
-echo "simrange : $simrange"
-echo "---------------------------------------------------------------------------------------"
+    srun --cpu-freq=2000000-2000000:performance likwid-perfctr -m -C M0:0-17 -g MEM_DP ./$perffile $simrange > $outfilename
+echo "---------------------------------------------------------------------------------------" | tee -a $finalres
+echo "simrange : $simrange" | tee -a $finalres
+echo "---------------------------------------------------------------------------------------" | tee -a $finalres
 
     while IFS= read -r line; do
     # Detect and extract region names
@@ -41,11 +49,11 @@ echo "--------------------------------------------------------------------------
         intensity=$(echo $line | awk -F'|' '{print $3}')
         
         # Output region and intensity
-        echo "Region: $region, Operational Intensity: $intensity [FLOP/Byte], Code balance: $(echo "1/$intensity" | bc -l) [Byte/FLOP]"
+        echo "Region: $region, Operational Intensity: $intensity [FLOP/Byte], Code balance: $(echo "1/$intensity" | bc -l) [Byte/FLOP]" | tee -a $finalres
     fi
 done < "$outfilename"
 
 done
 
-
+rm $perffile
 make clean
