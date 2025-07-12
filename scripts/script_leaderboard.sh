74130fe4c49c10b92e059635361a83ac4a2d7186
Sriram Katta <sriram.katta@fau.de>
2025-07-12 19:46:43 +0200

    leader script board added

--- scripts/script_leaderboard.sh
+++ scripts/script_leaderboard.sh
@@ -8,26 +8,18 @@
 
 unset SLURM_EXPORT_ENV
 
-numdoma=$(lscpu | grep "NUMA node(s)" | awk '{print $NF}')
-procs=$(nproc)
-cpustep=$(echo "$procs / $numdoma" | bc)
-cpustart=$cpustep
-numacpustart=$(echo "$cpustart + $cpustep" | bc)
-
 module load intel
 module load likwid
 
 DATE=$(date +'%d-%m-%y_%H@%M@%S')
 perffile=perf_$DATE
 
-LIKWID=off CXX=icpx make >/dev/null
 make clean
+LIKWID=off CXX=icpx make -j >/dev/null
 
 cp perf $perffile
 
-for numcores in $(seq $numacpustart $cpustep $procs); do
-    ./leaderboard.sh "" "likwid-pin -q -c E:N:$procs"
-done
+./leaderboard.sh "" "likwid-pin -q -c E:N:$(nproc)"
 
 rm $perffile
 make clean
